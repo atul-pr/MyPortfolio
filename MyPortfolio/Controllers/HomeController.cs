2f2bec1660de4b1a6db0d4dcfa41fec955fecbab
atul-pr <atulxcx@gmail.com>
2025-12-30 16:02:28 +0530

    Add project files.

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MyPortfolio.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+           
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ MyPortfolio/Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.Models
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TechStack { get; set; }
+        public string Icon { get; set; }
+    }
+}
